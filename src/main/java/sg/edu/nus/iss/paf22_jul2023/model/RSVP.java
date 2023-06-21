f8b148666d72b16e516693c8f84f905d0e078501
Darryl Ng <darryl1975@hotmail.com>
2023-06-21 14:07:35 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf22_jul2023/model/RSVP.java
@@ -0,0 +1,24 @@
+package sg.edu.nus.iss.paf22_jul2023.model;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RSVP {
+    private int id;
+
+    private String fullName;
+
+    private String email;
+
+    private Integer phone;
+
+    private Date confirmationDate;
+
+    private String comment;
+}
